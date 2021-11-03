23273ed75deaac53270954777ff7b9511dbedf51
Anvarzhonov <anvarzhonov@mail.ru>
2021-11-04 00:21:15 +0300

    added dependencies

--- src/main/java/ru/anvarzhonov/springbootwebsocket/SpringBootWebsocketApplication.java
+++ src/main/java/ru/anvarzhonov/springbootwebsocket/SpringBootWebsocketApplication.java
@@ -3,6 +3,9 @@ package ru.anvarzhonov.springbootwebsocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootWebsocketApplication {
 
